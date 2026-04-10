1a30935716725217b1f08121d55894c421166f79
jefersonsantana580 <jefin.souza.92@gmail.com>
2026-04-09 21:10:15 -0300

    Update Home.py

--- Home.py
+++ Home.py
@@ -3,7 +3,7 @@ import streamlit as st
 st.set_page_config(
     page_title="Central de Apps Streamlit",
     page_icon="📊",
-    layout="WIDE"
+    layout="wide"
 )
 
 st.title("📊 Ferramentas para Otimizaçãoo de trabalho - MPS")

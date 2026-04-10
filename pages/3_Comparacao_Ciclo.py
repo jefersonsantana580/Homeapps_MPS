b52ce74a4a3bc7e81c82959a701c85c51104d098
jefersonsantana580 <jefin.souza.92@gmail.com>
2026-04-09 22:38:20 -0300

    Update 3_Comparacao_Ciclo.py

--- pages/3_Comparacao_Ciclo.py
+++ pages/3_Comparacao_Ciclo.py
@@ -11,7 +11,7 @@ import streamlit as st
 # =====================================================
 
 st.set_page_config(
-    page_title="Comparativo Request Vs Plan",
+    page_title="Comparativo P.Request Vs O.Plan",
     layout="wide"
 )
 

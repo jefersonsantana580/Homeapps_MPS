9b7cfacb7cb7a14ebcd55b84d09b835c78e352aa
jefersonsantana580 <jefin.souza.92@gmail.com>
2026-04-15 09:35:15 -0300

    Update 2_NIvelar_com_Filas.py

--- pages/2_NIvelar_com_Filas.py
+++ pages/2_NIvelar_com_Filas.py
@@ -25,9 +25,27 @@ with col2:
             mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
         )
 
-# SIDEBAR PERSONALIZADA
-st.sidebar.image("images/agco.jpg")
+# === SIDEBAR (IGUAL AO HOME) ===
+st.markdown(
+    """
+    <style>
+        [data-testid="stSidebarNav"] {
+            display: none;
+        }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
+st.sidebar.image("images/agco.jpg", width=240)
 st.sidebar.divider()
+
+st.sidebar.markdown("### 📊 Aplicações")
+st.sidebar.page_link("Home.py", label="🏠 Home")
+st.sidebar.page_link("pages/1_Nivelamento.py", label="📈 Nivelamento sem filas")
+st.sidebar.page_link("pages/2_NIvelar_com_Filas.py", label="🛠 Nivelamento com Filas")
+st.sidebar.page_link("pages/3_Comparacao_Ciclo.py", label="🔄 Comparativo PR vs Plan")
+# =================================
 
 # =====================================================
 # Configuração da página
